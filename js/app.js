87663d6ec2f146125d5f385e809e7215d75b911c
Racknar0 <racknarow1@gmail.com>
2022-02-28 00:04:27 -0500

    Crete Buttons

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
 
 const registrosPorPagina = 40;
 let totalPaginas;
@@ -52,7 +53,7 @@ function mostarAlerta( mensaje ) {
 
 function buscarImagenes(termino){
     const key = '25914404-864d61f09b8f19f06954aec3b';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -103,8 +104,14 @@ function mostrarImagenes(imagenes) {
 
     })
 
+    //! limpiar paginadro previo
+    while(paginacionDiv.firstChild){
+        paginacionDiv.removeChild(paginacionDiv.firstChild)
+    }
+
+    //! generar nuevo html
     imprimirPaginador();
-    
+
 }
 
 
@@ -112,4 +119,17 @@ function imprimirPaginador() {
 
     iterador = crearPaginador(totalPaginas);
 
+    while (true) {
+        const {value, done} = iterador.next();
+        if(done) return;
+
+        //! genera un boton por cada elemento en el generador
+        const boton = document.createElement('A');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente' ,'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded');
+
+        paginacionDiv.appendChild(boton);
+    }
 }
